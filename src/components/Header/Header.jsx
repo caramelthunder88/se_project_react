ee5b004bf5d6547e9c33a6f33fed6647d392a92b
Donte <damorgan19@gmail.com>
2025-06-25 17:29:27 -0400

    feat: add toggle switch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import logo from "../../assets/wtwr.svg";
 import avatar from "../../assets/Ellipse 18.png";
 
@@ -15,13 +16,19 @@ function Header({ handleAddClick, weatherData }) {
         {currentDate} {weatherData.city}
       </p>
 
-      <button
-        onClick={handleAddClick}
-        type="button"
-        className="header__add-clothes-btn"
-      >
-        + Add clothes
-      </button>
+      <div className="header__controls">
+        <div className="header__toggle">
+          <ToggleSwitch />
+        </div>
+
+        <button
+          onClick={handleAddClick}
+          type="button"
+          className="header__add-clothes-btn"
+        >
+          + Add clothes
+        </button>
+      </div>
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="User" className="header__avatar" />
